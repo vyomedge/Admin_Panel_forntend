05069a56da8e70b0d58cd6d10438270e01322ebc
swapnil9340 <swapnilchauhan08@gmail.com>
2025-07-17 10:51:56 +0530

    changes

--- src/routes/routesConfig.js
+++ src/routes/routesConfig.js
@@ -1,20 +1,23 @@
-import Layout1 from '../layouts/Layout1';
-import Layout2 from '../layouts/Layout2';
-import RoutingList from '../components/RoutingList';
-import ProtectedRoute from '../components/ProtectedRoute';
-import Login from '../pages/Login';
-import Dashboard from '../pages/Dashboard';
-import PublicRoute from '../components/PublicRoute';
+import Layout1 from "../layouts/Layout1";
+import Layout2 from "../layouts/Layout2";
+import RoutingList from "../components/RoutingList";
+import ProtectedRoute from "../components/ProtectedRoute";
+import Login from "../pages/Login";
+import Dashboard from "../pages/Dashboard";
+import PublicRoute from "../components/PublicRoute";
+import Blog from "../pages/Blog";
 
 const routesConfig = [
   {
     element: <Layout1 />,
     children: [
       {
-        path: '/',
-        element:     <PublicRoute>
-          <RoutingList Component={Login} />
-        </PublicRoute>,
+        path: "/",
+        element: (
+          <PublicRoute>
+            <RoutingList Component={Login} />
+          </PublicRoute>
+        ),
       },
     ],
   },
@@ -26,8 +29,12 @@ const routesConfig = [
     ),
     children: [
       {
-        path: '/dashboard',
+        path: "/dashboard",
         element: <RoutingList Component={Dashboard} />,
+      },
+      {
+        path: "/blog",
+        element: <RoutingList Component={Blog} />,
       },
     ],
   },
